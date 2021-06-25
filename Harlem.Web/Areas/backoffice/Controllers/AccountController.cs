17f7bae48c0290eeeb15655864249868a4c5229f
Hasan Korkmaz <48335307+Hasan-Korkmaz@users.noreply.github.com>
2021-06-25 23:14:37 +0300

    Login View

---
+++ Harlem.Web/Areas/backoffice/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harlem.Web.Areas.backoffice.Controllers
+{
+    [Area("BackOffice")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public void Login(string userName,string password)
+        {
+            Response.Redirect("\\Backoffice\\Product\\Index");
+        }
+    }
+}
