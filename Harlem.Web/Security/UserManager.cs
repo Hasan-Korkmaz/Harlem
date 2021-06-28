8868110c2d9cec93d95d55ef5a928d37240fad12
Can Ahmet Ceylan <canahmetceylan@outlook.com>
2021-06-28 23:10:42 +0300

    UserManager

---
+++ Harlem.Web/Security/UserManager.cs
@@ -0,0 +1,72 @@
+﻿using Harlem.BLL.Abstract;
+using Harlem.Entity.DTO.Users;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Harlem.Web.Security
+{
+    public class UserManager
+    {
+        private readonly IUserService userService;
+
+        public UserManager(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<bool> SingIn(HttpContext httpContext, UserLoginDTO model, bool isPersistent = false)
+        {
+            var user = userService.CheckUser(model);
+
+            if (user != null)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    GetUserClaims(user),
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public async Task SignOut(HttpContext httpContext)
+        {
+            await httpContext.SignOutAsync();
+        }
+
+
+        private IEnumerable<Claim> GetUserClaims(UserDTO user)
+        {
+            List<Claim> claims = new List<Claim>();
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            claims.Add(new Claim(ClaimTypes.Name, user.Name));
+            claims.Add(new Claim(ClaimTypes.Surname, user.Surname));
+            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+
+            // Todo: Rol Kullanılacak İse bu kısım aktif edilecek
+            // claims.AddRange(this.GetUserRoleClaims(user));
+            return claims;
+        }
+
+        private IEnumerable<Claim> GetUserRoleClaims(UserDTO user)
+        {
+            List<Claim> claims = new List<Claim>();
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            // claims.Add(new Claim(ClaimTypes.Role, user.UserPermissionType.ToString()));
+            return claims;
+        }
+    }
+}
